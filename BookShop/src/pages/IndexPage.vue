f89d85827a189ebe1938851006c3d9f7db42b77c
DinoStojcevic <dinoskola0@gmail.com>
2024-12-05 11:07:56 +0100

    push

---
+++ BookShop/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>David Gušćić 5.12.2024.</h1>
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
+
+<style scoped>
+h1{
+  font-family: 'Courier New', Courier, monospace;
+  font-size:4rem;
+  text-decoration: underline;
+}
+</style>
